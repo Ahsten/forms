a493e07cdbbf7a8a50a49c51c97f09024a2e7966
Luxe17 <hanson1717@live.com>
2023-02-16 14:21:00 -0700

    Added edit update actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,12 @@ class UsersController < ApplicationController
         end
     end
 
+    def edit
+    end
+
+    def update
+    end
+
     private
         def user_params
             params.require(:user).permit(:username, :email, :password)
